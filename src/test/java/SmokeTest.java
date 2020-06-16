c16130b4f4706c54540233a82d9088ffa592ea82
jan-tyburski <jantyburski@gmail.com>
2020-06-16 14:07:48 +0200

    Added simple test.

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.AssertionInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        Character character = null;
+        Assertions.assertTrue(character == null);
+    }
+}
+
